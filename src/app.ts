d96e2a468bca81590f5be2145394e94ede8390e9
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-05-09 20:28:56 +0300

    Revert "lesson-6"
    This reverts commit 200266048dd5a9f5c986e06904fd08acbcc08237.

--- src/app.ts
+++ src/app.ts
@@ -51,34 +51,34 @@ function customError(): never {
   throw new Error("Error");
 }
 
-// type Page = {
-//   title: string;
-//   likes: number;
-//   accounts: string[];
-//   status: "open" | "close";
-//   details?: {
-//     createAt: string;
-//     updateAt: string;
-//   };
-// };
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: string;
+    updateAt: string;
+  };
+};
 
-// const page1: Page = {
-//   title: "The awesome page",
-//   likes: 100,
-//   accounts: ["Max", "Anton", "Nikita"],
-//   status: "open",
-//   details: {
-//     createAt: "2021-01-01",
-//     updateAt: "2021-05-01",
-//   },
-// };
+const page1: Page = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: "2021-01-01",
+    updateAt: "2021-05-01",
+  },
+};
 
-// const page2: Page = {
-//   title: "Python or Js",
-//   likes: 5,
-//   accounts: ["Alex"],
-//   status: "close",
-// };
+const page2: Page = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
 
 class Key {
   private signature: number;
@@ -134,48 +134,3 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
-function getPromise(): Promise<(string | number)[]> {
-  return new Promise((resolve) => {
-    resolve(["Text", 50]);
-  });
-}
-
-getPromise().then((data) => {
-  console.log(data);
-});
-
-type AllType = {
-  name: string;
-  position: number;
-  color: string;
-  weight: number;
-};
-function compare(
-  top: Pick<AllType, "name" | "color">,
-  bottom: Pick<AllType, "position" | "weight">
-): AllType {
-  return {
-    name: top.name,
-    color: top.color,
-    position: bottom.position,
-    weight: bottom.weight,
-  };
-}
-
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
-}
-
-class Component<T> {
-  constructor(public props: T) {}
-}
-
-interface IProps {
-  title: string;
-}
-class Page extends Component<IProps> {
-  pageInfo() {
-    console.log(this.props.title);
-  }
-}
